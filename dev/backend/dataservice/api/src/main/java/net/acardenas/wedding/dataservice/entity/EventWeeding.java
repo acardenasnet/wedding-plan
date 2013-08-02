9e15e9ef1b8450dfda67e125d614fbff7a2fe0db
Alejandro Cardenas <cardenas.alejandro@gmail.com>
2013-08-01 17:41:02 -0800

    add entitys

--- dev/backend/dataservice/api/src/main/java/net/acardenas/wedding/dataservice/entity/EventWeeding.java
+++ dev/backend/dataservice/api/src/main/java/net/acardenas/wedding/dataservice/entity/EventWeeding.java
@@ -1,5 +1,24 @@
-package net.acardenas.wedding.dataservice;
+package net.acardenas.wedding.dataservice.entity;
 
-public class EventWeeding {
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Max;
+
+@Entity
+public class EventWeeding
+{
+    private long id;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+    
+    @OneToMany(mappedBy="eventWeeding")
+    @Max(value = 2)
+    private List<Grooms> grooms;
 
 }
--- dev/backend/dataservice/api/src/main/java/net/acardenas/wedding/dataservice/entity/Grooms.java
+++ dev/backend/dataservice/api/src/main/java/net/acardenas/wedding/dataservice/entity/Grooms.java
@@ -1,4 +1,4 @@
-package net.acardenas.wedding.dataservice;
+package net.acardenas.wedding.dataservice.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
