cb25432b5203939d8dfcc61ed962d13bf0dfb447
Alejandro Cardenas <cardenas.alejandro@gmail.com>
2013-08-13 00:25:01 -0500

    Fix Login page

--- dev/web/src/main/java/net/acardenas/wedding/web/LoginBean.java
+++ dev/web/src/main/java/net/acardenas/wedding/web/LoginBean.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpSession;
 
 @ManagedBean
 @SessionScoped
-public class HelloBean implements Serializable
+public class LoginBean implements Serializable
 {
 
     private static final long serialVersionUID = 1L;
