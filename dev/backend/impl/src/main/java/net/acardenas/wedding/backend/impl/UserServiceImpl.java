5c97b3180e1b62c1f00e71e76f7ab723b5132422
Alejandro Cardenas <cardenas.alejandro@gmail.com>
2013-08-13 00:30:30 -0500

    Add UserService Impl

---
+++ dev/backend/impl/src/main/java/net/acardenas/wedding/backend/impl/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package net.acardenas.wedding.backend.impl;
+
+import java.util.List;
+
+import net.acardenas.wedding.backend.UserService;
+import net.acardenas.wedding.dataservice.entity.User;
+
+public class UserServiceImpl
+    implements UserService
+{
+
+    @Override
+    public void createUser(User aUser)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public User readUser(Integer anId)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<User> readUsers(List<Integer> anIds)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public User updateUser(User aUser)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean deleteUser(User aUser)
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
