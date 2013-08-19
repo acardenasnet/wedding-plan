90c3a0f604557bbb21569f3ec058c8da2ea6fb50
Alejandro Cardenas <cardenas.alejandro@gmail.com>
2013-08-19 00:27:40 -0500

    Fix the logger

---
+++ dev/web/src/main/java/net/acardenas/wedding/web/util/LoggerProducer.java
@@ -0,0 +1,34 @@
+// ---------------------------------------------------------------------------
+// COPYRIGHT Alejandro Cardenas, acardenas.net, Saltillo,Coah, MX 2013
+// All rights reserved.
+//
+// The Copyright to the computer program(s) herein is the property of
+// Alejandro Raul Cardenas
+// The program(s) may be used and/or copied only with the written
+// permission from Alejandro Cardenas, or in
+// accordance with the terms and conditions stipulated in the
+// agreement/contract under which the program(s) have been supplied.
+// ---------------------------------------------------------------------------
+
+package net.acardenas.wedding.util;
+
+import java.util.logging.Logger;
+
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+@Default
+public class LoggerProducer
+{
+
+    /**
+     * @param injectionPoint
+     * @return logger
+     */
+    @Produces
+    public Logger produceLogger(InjectionPoint injectionPoint)
+    {
+        return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+    }
+}
