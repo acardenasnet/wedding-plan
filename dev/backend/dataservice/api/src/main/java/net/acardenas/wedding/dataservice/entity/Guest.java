6df51c192459e50363f212ecf25cfcd92c0a726a
Alejandro Cardenas <cardenas.alejandro@gmail.com>
2013-12-03 16:18:42 -0600

    add attributes

--- dev/backend/dataservice/api/src/main/java/net/acardenas/wedding/dataservice/entity/Guest.java
+++ dev/backend/dataservice/api/src/main/java/net/acardenas/wedding/dataservice/entity/Guest.java
@@ -18,6 +18,9 @@ import javax.persistence.Entity;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
+import net.acardenas.wedding.dataservice.enums.GromsType;
+import net.acardenas.wedding.dataservice.enums.GuestType;
+
 @Entity
 @Cacheable(value = true)
 @DiscriminatorValue(value = "G")
@@ -34,6 +37,8 @@ public class Guest extends User
     public final static String TOTAL = "Guest.countGuestsTotal";
     
     private int quantity;
+    private GuestType guestType;
+    private GromsType gromsType;
     
     public Guest()
     {
@@ -49,5 +54,26 @@ public class Guest extends User
     {
         quantity = aQuantity;
     }
+
+    public GuestType getGuestType()
+    {
+        return guestType;
+    }
+
+    public void setGuestType(GuestType aGuestType)
+    {
+        guestType = aGuestType;
+    }
+
+    public GromsType getGromsType()
+    {
+        return gromsType;
+    }
+
+    public void setGromsType(GromsType aGromsType)
+    {
+        gromsType = aGromsType;
+    }
+    
 
 }
